500b7fd2e1f28e38c5d441b781b7adde7395a860
Ayesha-Akhtar-Saati <ayeshamuhammadakhtar684@gmail.com>
2025-05-13 22:55:29 +0500

    structure

--- PF project(2).cpp
+++ PF project(2).cpp
@@ -12,3 +12,45 @@ const int MAX_ITEMS = 20;
 const char FILENAME_USERS[] = "users.dat";
 const char FILENAME_ORDERS[] = "orders.dat";
 const char FILENAME_RIDERS[] = "riders.dat";
+
+// Structure definitions
+typedef struct {
+    int id;
+    char name[50];
+    char type[20]; // "user", "admin", or "rider"
+    char username[50];
+    char password[50];
+    char phone[15];
+    char address[100];
+} User;
+
+typedef struct {
+    int id;
+    char name[50];
+    float price;
+    char category[30];
+} MenuItem;
+
+typedef struct {
+    int itemId;
+    int quantity;
+} OrderItem;
+
+typedef struct {
+    int id;
+    int userId;
+    time_t orderTime;
+    OrderItem items[MAX_ITEMS];
+    int itemCount;
+    float total;
+    char status[30]; // "preparing", "ready", "assigned", "delivered"
+    int riderId;
+} Order;
+
+typedef struct {
+    int id;
+    int userId;
+    char status[30]; // "available", "on-delivery"
+    int currentOrderId;
+} Rider;
+
